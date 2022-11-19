1ae1b1a878706a27502ac6a6eeaf35703202036a
Tatevik Kalanchyan <tatev.kalanchyan@gamil.com>
2022-11-19 16:41:37 +0300

    zadacha10

--- zadacha10/Program.cs
+++ zadacha10/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input.Equals("exit"))
+    {
+        break;
+    }
+    else
+    {
+        if (int.TryParse(input, out int number) && (number <= 999) && (number >= 100))
+        {
+            Console.WriteLine("{0}->{1}", input, input[1]);
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не трехзначное число");
+        }
+    }
+}
