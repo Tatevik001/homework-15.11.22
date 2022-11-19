b980a87f455cd6cadb677c2d2db97e1a480a6e22
Tatevik Kalanchyan <tatev.kalanchyan@gamil.com>
2022-11-19 16:43:13 +0300

    zadacha13

--- zadacha13/Program.cs
+++ zadacha13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+string ThirdDigit(int number)
+{
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        return (number % 10).ToString();
+    }
+    else
+    {
+        return " Третьей цифры нет ";
+    }
+}
+Console.WriteLine(ThirdDigit(456));
--- zadacha15/Program.cs
+++ zadacha15/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15:  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine(Readint("Введите число от 1 до 7: "));
+string Readint(string message)
+{
+    Console.Write(message);
+    string stroka = Console.ReadLine();
+    if (int.TryParse(stroka, out int a))
+    {
+        if (a > 0 && a < 8)
+        {
+            if (a == 7 || a == 6)
+            {
+                Console.Write("Под цифрой " + a + " - Выходной ");
+            }
+            else
+            {
+                Console.Write("Под цифрой " + a + " - Рабочий ");
+            }
+        }
+        else
+        {
+            Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить ");
+        }
+        return "день";
+    }
+    else
+    {
+        return ("Нужно ввести число");
+    }
+}
+
